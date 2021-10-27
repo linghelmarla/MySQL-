5d12752814052e4416a7625808223704e03a2e98
linghel marla <linghel.marlacms@gmail.com>
2021-10-27 00:14:47 -0300

    exercicios finais

--- exercicios/exercicio5.sql
+++ exercicios/exercicio5.sql
@@ -32,26 +32,26 @@ insert into tb_categoria (tipo, desconto, promocao) values ("Hidraulica", false,
 
 -- Criando os Produtos  --
 insert into tb_produto (nome, preco, qtProduto, em_estoque, marca_conhecida, categoria_id) 
-values ("Cimento", 30.00, 60, true, true, 2);
-insert into tb_produto (nome, preco, qtProduto, em_estoque, entrega_em_casa, categoria_id) 
+values ("Alicate", 70.00, 60, true, true, 1);
+insert into tb_produto (nome, preco, qtProduto, em_estoque, marca_conhecida, categoria_id) 
 values ("tijolo", 23.00, 200, true, true, 2);
-insert into tb_produto (nome, preco, qtProduto, em_estoque, entrega_em_casa, categoria_id) 
-values ("Martelo", 40.00, 270, true, false, 1);
-insert into tb_produto (nome, preco, qtProduto, em_estoque, entrega_em_casa, categoria_id) 
-values ("Espelho", 70.00, 54, true, false, 3);
-insert into tb_produto (nome, preco, qtProduto, em_estoque, entrega_em_casa, categoria_id) 
-values ("Cabiameto Elétrico", 17.70, 100, true, false, 4);
-insert into tb_produto (nome, preco, qtProduto, em_estoque, entrega_em_casa, categoria_id) 
+insert into tb_produto (nome, preco, qtProduto, em_estoque, marca_conhecida, categoria_id)  
+values ("Marreta", 40.00, 270, true, false, 1);
+insert into tb_produto (nome, preco, qtProduto, em_estoque, marca_conhecida, categoria_id)  
+values ("Moldura", 70.00, 54, true, false, 3);
+insert into tb_produto (nome, preco, qtProduto, em_estoque, marca_conhecida, categoria_id)  
+values ("Fiaçao", 17.70, 100, true, false, 4);
+insert into tb_produto (nome, preco, qtProduto, em_estoque, marca_conhecida, categoria_id) 
 values ("Cano ", 45.90, 500, true, true, 5);
-insert into tb_produto (nome, preco, qtProduto, em_estoque, entrega_em_casa, categoria_id) 
-values ("Telha", 30.00, 60, true, true, 2);
-insert into tb_produto (nome, preco, qtProduto, em_estoque, entrega_em_casa, categoria_id) 
-values ("Argamassa", 20.00, 88, true, true, 2);
+insert into tb_produto (nome, preco, qtProduto, em_estoque, marca_conhecida, categoria_id) 
+values ("Massa Corrida", 30.00, 60, true, true, 2);
+insert into tb_produto (nome, preco, qtProduto, em_estoque, marca_conhecida, categoria_id) 
+values ("Cimento", 20.00, 88, true, true, 2);
 
--- comando select produto com valor maior que 50 --
+-- comando selecione produto com valor maior que 50 --
 select * from tb_produto where preco > 50.00;
 
--- comando select produto com valor entre 3 a 60 --
+-- comando selecione produto com valor entre 3 a 60 --
 select * from tb_produto where preco between 03.00 AND 60.00;
 
 -- Buscar produto com a letra C no nome --
@@ -61,7 +61,7 @@ select * from tb_produto where nome Like "%C%";
 select * from tb_produto inner join tb_categoria
 on tb_produto.categoria_id = tb_categoria.id;
 
--- Select produto de um determinado tipo --
+-- Selecione produto de um determinado tipo --
 select tb_produto.nome, tb_produto.preco, tb_produto.em_estoque, tb_categoria.tipo, tb_categoria.desconto, tb_categoria.promocao from tb_produto inner join tb_categoria 
 on tb_produto.categoria_id = tb_categoria.id
 where tb_categoria.id = 2;
--- exercicios/exercicio6.sql
+++ exercicios/exercicio6.sql
@@ -26,34 +26,34 @@ create table tb_curso (
 );
 
 -- Criando as Categorias --
-insert into tb_categoria(area, qtvagas, professor) values ("Informática", 82, "Maiury");
-insert into tb_categoria(area, qtvagas, professor) values ("Gastronomia", 40, "Safira");
-insert into tb_categoria(area, qtvagas, professor) values ("Moda", 112, "Danillo");
-insert into tb_categoria(area, qtvagas, professor) values ("Jornalismo", 100, "Darcy");
-insert into tb_categoria(area, qtvagas, professor) values ("Administração", 50, "André");
+insert into tb_categoria(area, qtvagas, professor) values ("Informatica", 42, "Rafael");
+insert into tb_categoria(area, qtvagas, professor) values ("Culinaria", 40, "Tia Jess");
+insert into tb_categoria(area, qtvagas, professor) values ("Artes Cenicas", 82, "Jeffrey");
+insert into tb_categoria(area, qtvagas, professor) values ("Pedagogia", 100, "Wesley");
+insert into tb_categoria(area, qtvagas, professor) values ("Contabilidade", 60, "André");
 
--- Criando as Frutas  --
+-- Criando os Cursos  --
 insert into tb_curso(nome, preco, tempo_de_duracao_em_meses, em_promocao, certificado, precoformatura, categoria_id)
-values ("Programação em Java", 112.50, 6, true, true, 120.00, 1);
+values ("Programação em POO", 562.50, 6, false, true, 120.00, 1);
 insert into tb_curso(nome, preco, tempo_de_duracao_em_meses, em_promocao, certificado, precoformatura, categoria_id)
-values ("Alimentação Saudável", 45.99, 3, false, false, null, 2);
+values ("Curso de Churrasco", 145.99, 3, false, false, null, 2);
 insert into tb_curso(nome, preco, tempo_de_duracao_em_meses, em_promocao, certificado, precoformatura, categoria_id)
-values ("Contraste das Cores", 65.00, 6, false, false, 30.00, 3);
+values ("Atuaçao", 165.00, 6, false, false, 100.00, 3);
 insert into tb_curso(nome, preco, tempo_de_duracao_em_meses, em_promocao, certificado, precoformatura, categoria_id)
-values ("Redação", 15.00, 1, false, false, null, 4);
+values ("Libras", 15.00, 1, false, false, null, 4);
 insert into tb_curso(nome, preco, tempo_de_duracao_em_meses, em_promocao, certificado, precoformatura, categoria_id)
-values ("Administração Financeira", 600.50, 12, true, true, 390.00, 5);
+values ("Tributos", 600.50, 12, true, true, 390.00, 5);
 insert into tb_curso(nome, preco, tempo_de_duracao_em_meses, em_promocao, certificado, precoformatura, categoria_id)
-values ("Administração de Estoque", 475.50, 8, true, true, 260.00, 5);
+values ("Controle de Caixa", 475.50, 8, true, true, 260.00, 5);
 insert into tb_curso(nome, preco, tempo_de_duracao_em_meses, em_promocao, certificado, precoformatura, categoria_id)
 values ("CSS", 300.00, 6, true, true, 120.00, 1);
 insert into tb_curso(nome, preco, tempo_de_duracao_em_meses, em_promocao, certificado, precoformatura, categoria_id)
-values ("Alimentação Vegana", 110.50, 3, true, true, 20.00, 2);
+values ("Confeiteiro", 1110.50, 3, true, true, 20.00, 2);
 
--- comando select curso com valor maior que 50 --
+-- comando selecione curso com valor maior que 50 --
 select * from tb_curso where preco > 50.00;
 
--- comando select curso com valor entre 3 a 60 --
+-- comando selecione curso com valor entre 3 a 60 --
 select * from tb_curso where preco between 03.00 AND 60.00;
 
 -- Buscar curso com a letra C no nome --
@@ -63,7 +63,7 @@ select * from tb_curso where nome Like "%J%";
 select * from tb_curso inner join tb_categoria
 on tb_curso.categoria_id = tb_categoria.id;
 
--- Select curso de um determinado tipo --
+-- Selecione curso de um determinado tipo --
 select tb_curso.nome, tb_curso.preco, tb_curso.tempo_de_duracao_em_meses, tb_categoria.area, tb_categoria.qtvagas, tb_categoria.professor from tb_curso inner join tb_categoria 
 on tb_curso.categoria_id = tb_categoria.id
 where tb_categoria.id = 5;
